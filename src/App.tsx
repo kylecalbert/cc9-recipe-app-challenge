c902c5e58e786089adc5678d5c9f042a453de09d
Aneesa <aneesakhan.97@outlook.com>
2023-06-12 14:41:28 +0100

    uploaded recipe API data to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import { AuthContextProvider } from './context/AuthContext';
 import { createBrowserRouter, createRoutesFromElements, Route,RouterProvider, Outlet } from 'react-router-dom';
@@ -10,8 +10,8 @@ import Home from './components/Home';
 function App() {
 
   const APP_ID = '6792d8f2';
-  const APP_KEY = '2a44e00a924467ce710d1e6f28e9b96'
-  const exampleReq = ``
+  const APP_KEY = '2a44e00a924467ce710d1e6f28e9b96';
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
@@ -22,10 +22,25 @@ function App() {
     )
   );
 
-  return (
-    <AuthContextProvider>
-      <RouterProvider router={router} />
-    </AuthContextProvider>
+  useEffect (() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=6792d8f2&app_key=92a44e00a924467ce710d1e6f28e9b96`);
+    const data = response.json();
+    console.log(data);
+  }
+   return (
+    <div className="App">
+      <form className="search-form" action="">
+      <input className="search-bar" type="text" />
+      <button className="search-button" type="submit">Search</button>
+      </form>
+    </div>
+    // <AuthContextProvider>
+    //   <RouterProvider router={router} />
+    // </AuthContextProvider>
   );
 }
 
