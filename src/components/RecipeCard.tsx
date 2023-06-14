3e23085693230ec08cf1436b29ae563a904ab151
Aneesa <aneesakhan.97@outlook.com>
2023-06-14 11:46:16 +0100

    created recipe grid

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -4,16 +4,16 @@ import { Favorite, } from '@mui/icons-material';
 
 function RecipeCard({title, calories, image}:any) {
   return (
-    <Box sx={{ maxWidth: 345, bgcolor: 'light', boxShadow: 1, margin: 2 }}>
+    <Box sx={{ maxWidth: 345, bgcolor: 'light', boxShadow: 1, color: 'black', margin: 2 }}>
       <CardMedia
         component="img"
         height="194"
       /> {image}
         <CardContent>
-        <Typography variant="h5" color="text.secondary">
+        <Typography variant="h5">
         {title}
         </Typography>
-        <Typography variant="h6" color="text.secondary">
+        <Typography variant="h6">
         {calories}
         </Typography>
       </CardContent>
--- src/components/RecipeCardGrid.tsx
+++ src/components/RecipeCardGrid.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import {Box,  Paper, Grid}  from '@mui/material/';
+import RecipeCard from './RecipeCard';
 
-function RecipeCardGrid() {
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function ResponsiveGrid() {
   return (
-    <div></div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {RecipeCard.map((_, index) => (
+          <Grid xs={2} sm={4} md={4} key={index}>
+            <Item>xs=2</Item>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 }
-
-export default RecipeCardGrid
