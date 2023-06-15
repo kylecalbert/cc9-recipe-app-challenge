7c6247fb989153f8de0e5a2c2ba4010e1ffdf937
Aneesa <aneesakhan.97@outlook.com>
2023-06-15 14:59:36 +0100

    added search function with use effect

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,12 +4,27 @@ import { useRecipeContext } from '../context/RecipeContext';
 import RecipeCardGrid from './RecipeCardGrid';
 
 function Home() {
+  const [ search, setSearch ] = useState<string>("");
+  const {recipes } = useRecipeContext();
+  const [ query, setQuery ] = useState<string>("");
 
+  const updateSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  }
 
+ useEffect(()=>{
+
+},[query])
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setQuery(search);
+  }
+  
   return (
     <div className="App">
-      <form className="search-form" action="">
-        <input className="search-bar" type="text" />
+      <form className="search-form" onSubmit={handleSearch}>
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
         <button className="search-button" type="submit">
           Search
         </button>
